5f1adb3704d6225e54cef8f9d235a80ec8aef30e
Masato Sumita <46772738+upyo-ms@users.noreply.github.com>
2022-05-08 12:44:25 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -12,23 +12,27 @@ except:
 
 #if len(sys.argv)==3: option = sys.argv[2]
 
-#For reading log file ####
+#######For reading log file ####
 #option = "eae homolumo"
-#test_sdf = GaussianRunPack.GaussianDFTRun('B3LYP', 'STO-3G', 12, option,infilename,0)
-#outdic = test_sdf.Extract_values(infilename,0,0,0,0,0,1,1,0,0,0,0)
+#option_array = [1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#bond1 = []
+#bond2 = []
+#test_sdf = GaussianRunPack.GaussianDFTRun('B3LYP', 'STO-3G', 12, option, infilename)
+#outdic = test_sdf.Extract_values(infilename,option_array, bond1 , bond2 )
 #print (outdic)
-#########################
+###################################
 
 
 #for stargint from gaussian calculation
 #option = "symm uv"
-#option = "opt deen nmr stable2o2"
+option = "opt Freq"
 #option = "opt deen vip vea homolumo dipole uv symm"
-#option = "uv=/home/sumita/GaussianRun_2.1beta/Samples/UV_peak.dat" 
-#option = "opt homolumo energy dipole deen stable2o2 uv=/home/sumita/GaussianRun_2.1beta/Samples/UV_peak.dat vip vea aip aea" 
+#option = "symm opt freq nmr uv=/home/sumita/GaussianRun_2.2beta/Samples/UV_peak.dat" 
+#option = "opt homolumo energy dipole deen stable2o2 uv=/home/sumita/GaussianRun_2.2beta/Samples/UV_peak.dat vip vea aip aea" 
 #option = "opt homolumo energy dipole deen stable2o2 uv vip vea aip aea" 
-option = "opt homolumo energy dipole deen stable2o2 fluor=3" 
+#option = "opt homolumo energy dipole deen stable2o2 fluor=3" 
 #option = "opt cden homolumo energy dipole deen stable2o2 tadf" 
+#option = "opt nmr uv energy homolumo dipole deen stable2o2 vip vea cden symm"
 #solvent = "water"
 
 test_sdf = GaussianRunPack.GaussianDFTRun('B3LYP', 'STO-3G', 8, option, infilename)
@@ -37,10 +41,10 @@ test_sdf.mem = '5GB'
 test_sdf.timexe = 60*60
 outdic = test_sdf.run_gaussian()
 
-del test_sdf
-gc.collect()
+#del test_sdf
+#gc.collect()
 
-print(os.getcwd())
+#print(os.getcwd())
 
 print (outdic)
 #######################################
