4256e5cb999c83028cec181425e384f4968fe9df
Masato Sumita <46772738+upyo-ms@users.noreply.github.com>
2022-08-25 13:37:55 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -24,18 +24,17 @@ except:
 
 
 #for stargint from gaussian calculation
-option = "symm"
-#option = "opt  stable2o2"
-#option = "opt deen vip vea homolumo dipole uv symm"
+#option = "opt pka symm"
+#option = "opt uv vip vea"
+option = "opt deen vip vea homolumo dipole uv symm"
 #option = "symm opt freq nmr uv=/home/sumita/GaussianRun_2.2beta/Samples/UV_peak.dat" 
 #option = "opt homolumo energy dipole deen stable2o2 uv=/home/sumita/GaussianRun_2.2beta/Samples/UV_peak.dat vip vea aip aea" 
 #option = "opt homolumo energy dipole deen stable2o2 uv vip vea aip aea" 
 #option = "opt homolumo energy dipole deen stable2o2 fluor=3" 
 #option = "opt cden homolumo energy dipole deen stable2o2 tadf" 
 #option = "opt nmr uv energy homolumo dipole deen stable2o2 vip vea cden symm"
-#solvent = "water"
 
-test = GaussianRunPack.GaussianDFTRun('B3LYP', 'STO-3G', 8, option, infilename, restart=True)
+test = GaussianRunPack.GaussianDFTRun('B3LYP', 'STO-3G', 10, option, infilename, solvent='water', restart=False)
 
 test.mem = '5GB'
 test.timexe = 60*60
