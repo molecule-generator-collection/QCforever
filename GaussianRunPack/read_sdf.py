3fc253fcf50faa27a38daad648fdb5b1b7c9af15
sishida21 <29348731+sishida21@users.noreply.github.com>
2022-12-02 16:39:11 +0900

    refactor: refactor read_sdf.py

--- GaussianRunPack/read_sdf.py
+++ GaussianRunPack/read_sdf.py
@@ -1,14 +1,13 @@
-import gc, sys
-from numpy import *
+import gc
+import sys
+import numpy as np
 from rdkit import Chem
-from rdkit.Chem import AllChem, Descriptors, rdmolops
+from rdkit.Chem import Descriptors, rdmolops
+
 
 def read_sdf(infile):
-
     with open(infile, 'r') as ifile:
-
         sdfinfo = Chem.SDMolSupplier(infile, removeHs=False)
-        
         for mol in sdfinfo:
             N = mol.GetNumAtoms()
             N_Bond = mol.GetNumBonds()
@@ -16,49 +15,37 @@ def read_sdf(infile):
             Charge = rdmolops.GetFormalCharge(mol) 
         
         count = 0
-        
         X = []
         Y = []
         Z = []
         element_symbol = []
-        
         Bond_pair1 = []
         Bond_pair2 = []
         Bond_type = []
-        
         sdfCharge = 0 
         CHG_atom = []
         CHG = []
-
         sdfSpinMulti = 0
         RAD_atom =[]
         RAD = []
         
         for line in ifile:
             if count == 0:
-                Header1 = line
-        
+                Header1 = line  # not used
                 count += 1
                 continue
-        
             if count == 1:
-                Header2 = line
-        
+                Header2 = line  # not used
                 count += 1
                 continue
-        
             if count == 2:
-                Header3 = line
-        
+                Header3 = line  # not used
                 count += 1
                 continue
-        
             if count == 3:
-                a = line.split()
-        
+                a = line.split()  # not used
                 count += 1
                 continue
-        
             if 3 < count <= N+4:
                 i_atom = line.split()
                 if len(i_atom) != 0:
@@ -66,20 +53,16 @@ def read_sdf(infile):
                     Y.append(float(i_atom[1]))
                     Z.append(float(i_atom[2]))
                     element_symbol.append(i_atom[3])
-        
                 count += 1
                 continue
-        
-            if N+4 < count <= N+N_Bond+3 :
+            if N+4 < count <= N+N_Bond+3:
                 bond_info = line.split()
                 bond_info = line.split()
                 Bond_pair1.append(int(bond_info[0]))
                 Bond_pair2.append(int(bond_info[1]))
                 Bond_type.append(int(bond_info[2]))
-        
-                count +=1
+                count += 1
                 continue
-        
             if count > N+N_Bond+3:
                 mol_info = line.split()
                 if (mol_info[0] == "M"):
@@ -100,27 +83,21 @@ def read_sdf(infile):
                 else:
                     print("The sdf file is invalid!")
                     sys.exit()
-        
                 count +=1
         
-###########Copy to array of numpy###########################
-
+    # Copy to array of numpy
     Mol_atom = []
-    Mol_CartX = zeros(N)
-    Mol_CartY = zeros(N)
-    Mol_CartZ = zeros(N)
-
-    CHG_atom = array(CHG_atom)
-    CHG = array(CHG)
-
+    Mol_CartX = np.zeros(N)
+    Mol_CartY = np.zeros(N)
+    Mol_CartZ = np.zeros(N)
+    CHG_atom = np.array(CHG_atom)
+    CHG = np.array(CHG)
     for j in range(N):
         Mol_CartX[j] = X[j] 
         Mol_CartY[j] = Y[j] 
         Mol_CartZ[j] = Z[j] 
         Mol_atom.append(element_symbol[j])
-
-#del element_symbol[N:TotalStep]
-
+    # del element_symbol[N:TotalStep]
     del element_symbol[:]
     del X[:]
     del Y[:]
@@ -128,20 +105,14 @@ def read_sdf(infile):
     gc.collect()
 
     print('Reading the sdf file has finished')
-
-    SpinMulti = N_radical+1
-
+    SpinMulti = N_radical + 1
     return Mol_atom, Mol_CartX, Mol_CartY, Mol_CartZ, Charge, SpinMulti, Bond_pair1, Bond_pair2
 
 
 if __name__ == '__main__':
-        
     usage = 'Usage; %s sdf_file' % sys.argv[0]
-
     try:
         infilename = sys. argv[1]
     except:
         print (usage); sys.exit()
-
     print (read_sdf(infilename))
-
