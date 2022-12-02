27a19537747775f1d1d9e74687a7e46ddc40bece
sishida21 <29348731+sishida21@users.noreply.github.com>
2022-12-02 13:38:43 +0900

    refactor: refactor CF_1D.py

--- GaussianRunPack/CF_1D.py
+++ GaussianRunPack/CF_1D.py
@@ -1,37 +1,31 @@
-import sys, math
-import copy
-from numpy import *
+import numpy as np
+
 
 def Integral(x, F):
-
     Int_F = 0
-
     for i in range(len(F)-1):
-        AvFi = (F[i] + F[i+1])/2
-        Int_F += (x[i+1]-x[i])*AvFi
-
+        AvFi = (F[i]+F[i+1]) / 2
+        Int_F += (x[i+1]-x[i]) * AvFi
     return Int_F
 
+
 def Corr_func(fx, f, gx = [], g = []):
-    
+    """
+    Normalization
+    Int_CF = Integral(fx, CF)
+    CF = CF / Int_CF
+    """
     if g == [] and gx == []:
         gx = fx.copy()
         g = f.copy()
-
-    CF = zeros(len(f))
-
+    CF = np.zeros(len(f))
     for i in range(len(f)):
         for j in range(len(g)-i):
-               CF[i] += f[j]*g[j+i]
-
-####Normailization##############
-#    Int_CF = Integral(fx, CF)
-#
-#    CF = CF/Int_CF
-################################
-
+               CF[i] += f[j] * g[j+i]
     return fx, CF
 
+
+# This function is not working. Need to check.
 def write_TCF(CFx, CF):        
 
     ofile_CF = open('CF.dat', 'w')
