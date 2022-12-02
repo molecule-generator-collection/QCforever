f1bdcb2d34ce8562e34baf0a666532c40b928bda
sishida21 <29348731+sishida21@users.noreply.github.com>
2022-12-02 16:37:05 +0900

    refactor: refactor Get_MolCoordinate.py

--- GaussianRunPack/Get_MolCoordinate.py
+++ GaussianRunPack/Get_MolCoordinate.py
@@ -1,55 +1,45 @@
-import os, sys, math
-import re, gc
+import sys
+import gc
 from numpy import *
+
 import GaussianRunPack.AtomInfo
 
+
 def Make_xyzfile(atom, X, Y, Z, ofile, charge, spin):
-
     s = ""
-    s += str(len(atom)) + "\n"
-    s += str(charge)+" "+ str(spin)+"\n"
+    s += f"{len(atom)}\n"
+    s += f"{charge} {spin}\n"
+    with open(ofile,'w') as f:
+        f.write(s)
+        for i in range(len(atom)):
+            f.write('%-4s % 10.5f % 10.5f % 10.5f \n' % (atom[i], X[i], Y[i], Z[i]))
 
-    ofile = open(ofile,'w')
-
-    ofile.write(s)
-
-    for i in range(len(atom)):
-        ofile.write('%-4s % 10.5f % 10.5f % 10.5f \n' % (atom[i], X[i], Y[i], Z[i]))
-
-    ofile.close()
 
 def MaxBondLength(lines, bondpair1, bondpair2):
-
-    Mol_atom, Mol_X, Mol_Y, Mol_Z = Extract_Coordinate(lines, None)
-
+    _, Mol_X, Mol_Y, Mol_Z = Extract_Coordinate(lines, None)
     BondLength = zeros(len(bondpair1))
-    BondYN = zeros(len(bondpair1))
-
+    BondYN = zeros(len(bondpair1))  # not used
     for i in range(len(bondpair1)):
-        BondLength[i] = sqrt((Mol_X[int(bondpair2[i])-1] - Mol_X[int(bondpair1[i])-1])**2+ \
-                             (Mol_Y[int(bondpair2[i])-1] - Mol_Y[int(bondpair1[i])-1])**2+ \
-                             (Mol_Z[int(bondpair2[i])-1] - Mol_Z[int(bondpair1[i])-1])**2)
-
+        BondLength[i] = sqrt((Mol_X[int(bondpair2[i])-1] - Mol_X[int(bondpair1[i])-1])**2
+                             + (Mol_Y[int(bondpair2[i])-1] - Mol_Y[int(bondpair1[i])-1])**2
+                             + (Mol_Z[int(bondpair2[i])-1] - Mol_Z[int(bondpair1[i])-1])**2)
     return  amax(BondLength)
 
 
 def Extract_Coordinate(lines, outfile=None):
-
     print ("Start finding coordinates...")
-
     Atom_index = []
     NumElement = []
     AtomicType = []
     X = []
     Y = []
     Z = []
-
     count = 0
     count_Station = 0
     Total_NumStation =0
     Index_Station = 0
 
-######Counting Stationary points##############
+    # Counting Stationary points
     for line in lines:
         if line.find("Charge =") >= 0:
             line_charge_spin = line.split()
@@ -57,9 +47,8 @@ def Extract_Coordinate(lines, outfile=None):
             given_SpinMulti = int(line_charge_spin[-1])
         if line.find("-- Stationary point found.") >= 0:
             count_Station += 1
-##############################################
-    Total_NumStation = count_Station
 
+    Total_NumStation = count_Station
     if Total_NumStation > 0:
         count_Station = 0
         print ("Total number of stationary points: ", Total_NumStation)
@@ -70,33 +59,26 @@ def Extract_Coordinate(lines, outfile=None):
                 print ("A stationary point is found! #", Index_Station)
                 continue
             if line.find("Standard orientation:") >=0 and count_Station > 0:
-#                    print ("Standard orientaion was found")
-#                    print ("Start reading coordinate")
                 count += 1
                 continue
             if count == 1:
-                Border_1 = line
-#                    print (Border_1)
+                Border_1 = line  # not used
                 count += 1
                 continue
             if count == 2:
-                Index_1 = line
-#                    print (Index_1)
+                Index_1 = line  # not used
                 count += 1
                 continue
             if count == 3:
-                Index_2 = line
-#                    print (Index_2)
+                Index_2 = line  # not used
                 count += 1
                 continue
             if count == 4:
-                Border_2 = line
-#                    print (Border_2)
+                Border_2 = line  # not used
                 count += 1
                 continue
             if count >= 5:
                 i_atom = line.split()
-#                    print (i_atom)
                 if len(i_atom) == 6:
                     Atom_index.append(int(i_atom[0]))
                     NumElement.append(int(i_atom[1]))
@@ -113,7 +95,6 @@ def Extract_Coordinate(lines, outfile=None):
                     count = 0
                     count_Station = 0
                 continue
-
     else:
         for line in lines:
             if line.find("Standard orientation:") >=0:
@@ -122,28 +103,23 @@ def Extract_Coordinate(lines, outfile=None):
                 count += 1
                 continue
             if count == 1:
-                Border_1 = line
-#                    print (Border_1)
+                Border_1 = line  # not used
                 count += 1
                 continue
             if count == 2:
-                Index_1 = line
-#                    print (Index_1)
+                Index_1 = line  # not used
                 count += 1
                 continue
             if count == 3:
-                Index_2 = line
-#                    print (Index_2)
+                Index_2 = line  # not used
                 count += 1
                 continue
             if count == 4:
-                Border_2 = line
-#                    print (Border_2)
+                Border_2 = line  # not used
                 count += 1
                 continue
             if count >= 5:
                 i_atom = line.split()
-#                    print (i_atom)
                 if len(i_atom) == 6:
                     Atom_index.append(int(i_atom[0]))
                     NumElement.append(int(i_atom[1]))
@@ -160,21 +136,17 @@ def Extract_Coordinate(lines, outfile=None):
                     count = 0
                 continue
 
-
-#Translating  atomic number to element symbol
+    # Translating  atomic number to element symbol
     Mol_atom = []
     Mol_CartX = zeros(N)
     Mol_CartY = zeros(N)
     Mol_CartZ = zeros(N)
-
     for i in range(N):
         Mol_atom.append(GaussianRunPack.AtomInfo.AtomicNumElec(NumElement[i]))
         Mol_CartX[i] = X[i]
         Mol_CartY[i] = Y[i]
         Mol_CartZ[i] = Z[i]
-#            print (Mol_atom[i])
 
-#############
     del Atom_index[:]
     del NumElement[:]
     del AtomicType[:]
@@ -190,20 +162,11 @@ def Extract_Coordinate(lines, outfile=None):
 
 
 if __name__ == '__main__':
-
     usage = 'Usage; %s infilename' % sys.argv[0]
-
     try:
         infilename = sys. argv[1]
     except:
         print (usage); sys.exit()
-
-
     with open(infilename, 'r') as ifile:
         lines = ifile.readlines()
-
-       
     print(Extract_Coordinate(lines))
-
-
-
