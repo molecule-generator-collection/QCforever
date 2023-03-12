3a922a69e80b1f8530b77db0972656373e277f2f
upyo-ms <masato.sumita@riken.jp>
2023-03-12 14:59:49 +0900

    The way to get molecular internal coordinate is modified

--- qcforever/gaussian_run/Get_MolInterCoordinate.py
+++ qcforever/gaussian_run/Get_MolInterCoordinate.py
@@ -1,6 +1,5 @@
 import sys
 import re
-
 import numpy as np
 
 
@@ -27,6 +26,9 @@ def Extract_InterMol(lines, outfile=None):
                 Init_BondLength    = []
                 Init_BondAngle     = []
                 Init_DihedralAngle = []
+                BL = []
+                BA = []
+                DA = []
                 ifrag += 1
                 icount += 1
                 print ("Initial internal coordinate parameters were found.")
@@ -40,10 +42,13 @@ def Extract_InterMol(lines, outfile=None):
                 if len(i_intermol) > 2:
                     if re.search('R',i_intermol[1]):
                         Init_BondLength.append(float(i_intermol[3]))
+                        BL.append(re.sub(r'[A-Z()]', '', i_intermol[2]))
                     if re.search('A',i_intermol[1]):
                         Init_BondAngle.append(float(i_intermol[3]))
+                        BA.append(re.sub(r'[A-Z()]', '', i_intermol[2]))
                     if re.search('D',i_intermol[1]):
                         tmp_D = float(i_intermol[3])
+                        DA.append(re.sub(r'[A-Z()]', '', i_intermol[2]))
                         if abs(tmp_D)/180 < 1:
                             Init_DihedralAngle.append(tmp_D)
                         else:
@@ -92,30 +97,47 @@ def Extract_InterMol(lines, outfile=None):
                 continue
     else:
         print("Stational points are not found!")
-        Init_BondLength = [0.0, 0.0]
-        Init_BondAngle = [0.0, 0.0]
-        Init_DihedralAngle = [0.0, 0.0]  
-        Final_BondLength = [0.0, 0.0]
-        Final_BondAngle = [0.0, 0.0]
-        Final_DihedralAngle = [0.0, 0.0]
 
+    MaxDisplacementB = 0.0
+    MaxDisplacementA = 0.0
+    MaxDisplacementD = 0.0
 
-    Init_BondLength     = np.array(Init_BondLength)
-    Init_BondAngle      = np.array(Init_BondAngle)
-    Init_DihedralAngle  = np.array(Init_DihedralAngle)
-    Final_BondLength    = np.array(Final_BondLength)
-    Final_BondAngle     = np.array(Final_BondAngle)
-    Final_DihedralAngle = np.array(Final_DihedralAngle)
+    if BL != []:
+        Init_BondLength     = np.array(Init_BondLength)
+        Final_BondLength    = np.array(Final_BondLength)
+        DisplacementB = Final_BondLength -  Init_BondLength
+        MaxDisplacementB = DisplacementB.max()
+        iMaxB = BL[DisplacementB.argmax()]
 
-    DisplacementB = Final_BondLength -  Init_BondLength
-    DisplacementA = Final_BondAngle -  Init_BondAngle
-    DisplacementD = Final_DihedralAngle -  Init_DihedralAngle
-    for i in range(len(DisplacementD)):
-        if abs(DisplacementD[i]) > 180:
-            DisplacementD[i] = (360-abs(DisplacementD[i]))
-    MaxDisplacementB = max(DisplacementB)
-    MaxDisplacementA = max(DisplacementA)
-    MaxDisplacementD = max(DisplacementD)
+    if BA != []:
+        Init_BondAngle      = np.array(Init_BondAngle)
+        Final_BondAngle     = np.array(Final_BondAngle)
+        DisplacementA = Final_BondAngle -  Init_BondAngle
+        MaxDisplacementA = DisplacementA.max()
+        iMaxA = BA[DisplacementA.argmax()]
+
+    if DA != []:
+        Init_DihedralAngle  = np.array(Init_DihedralAngle)
+        Final_DihedralAngle = np.array(Final_DihedralAngle)
+        DisplacementD = Final_DihedralAngle -  Init_DihedralAngle
+
+        for i in range(len(DisplacementD)):
+            if abs(DisplacementD[i]) > 180:
+                DisplacementD[i] = (360-abs(DisplacementD[i]))
+
+        MaxDisplacementD = DisplacementD.max()
+        iMaxD = DA[DisplacementD.argmax()]
+
+    if outfile != None:
+        out_line =''
+        for i in range (len(BL)):
+            out_line += f'{BL[i]:10s} {Init_BondLength[i]: 10.5f} {Final_BondLength[i]: 10.5f}\n'
+        for i in range (len(BA)):
+            out_line += f'{BA[i]:10s} {Init_BondAngle[i]: 10.5f} {Final_BondAngle[i]: 10.5f}\n'
+        for i in range (len(DA)):
+            out_line += f'{DA[i]:10s} {Init_DihedralAngle[i]: 10.5f} {Final_DihedralAngle[i]: 10.5f}\n'
+        with open(outfile, 'w') as f:
+            f.write(out_line)
 
     return [MaxDisplacementB, MaxDisplacementA, MaxDisplacementD]
 
@@ -128,4 +150,4 @@ if __name__ == '__main__':
         print (usage); sys.exit()
     with open(infilename, 'r') as ifile:
         lines = ifile.readlines()
-    print(Extract_InterMol(lines))
+    print(Extract_InterMol(lines,'internalcoord.txt'))
