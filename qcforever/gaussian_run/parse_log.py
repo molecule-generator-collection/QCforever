4c28156a836a658502908163b4e15c5a4b41d509
upyo-ms <masato.sumita@riken.jp>
2023-12-26 14:51:01 +0900

    the method to extract the result of vibration analysis is added to parse_log.py

--- qcforever/gaussian_run/parse_log.py
+++ qcforever/gaussian_run/parse_log.py
@@ -352,8 +352,6 @@ class parse_log:
         
         CD_length, CD_OS = self.extract_CD(lines)
         mu, theta, g = self.extract_transtion_EMmoment(lines)
-        #State_allowed, State_forbidden, WL_allowed, WL_forbidden, OS_allowed, OS_forbidden, \
-        #    CD_L_allowed, CD_L_forbidden, CD_OS_allowed, CD_OS_forbidden = self.classify_SpinStates(Ideal_SS, SS, WaveLength, V_OS, CD_length, CD_OS)
         State_allowed, State_forbidden, WL_allowed, WL_forbidden, OS_allowed, OS_forbidden, \
                 CD_L_allowed, CD_L_forbidden, CD_OS_allowed, CD_OS_forbidden, \
                 mu_allowed, mu_forbidden, theta_allowed, theta_forbidden, g_allowed, g_forbidden \
@@ -363,7 +361,120 @@ class parse_log:
         return Found, Egrd, Eext, State_allowed, State_forbidden, WL_allowed, WL_forbidden, OS_allowed, OS_forbidden, \
             CD_L_allowed, CD_L_forbidden, CD_OS_allowed, CD_OS_forbidden, \
             mu_allowed, mu_forbidden, theta_allowed, theta_forbidden, g_allowed, g_forbidden  
+
+    def extract_Freq(self, lines):
+        count = 0
+        Freq = []
+        IR_int = []
+        Raman_int = []
+
+        print ("Get information about  Frequencies")
+        for line in lines:
+            if line.find("Frequencies --") >= 0:
+                line_Freq = line.split()
+                for i in range(2, len(line_Freq)):
+                    Freq.append(float(line_Freq[i]))
+            if line.find("IR Inten    --") >= 0:
+                line_ir = line.split()
+                for i in range(3, len(line_ir)):
+                    IR_int.append(float(line_ir[i]))
+            if line.find("Raman Activ --") >= 0:
+                line_raman = line.split()
+                for i in range(3, len(line_raman)):
+                    Raman_int.append(float(line_raman[i]))
+            if line.find("Sum of electronic and zero-point Energies=") >= 0:
+                line_zp = line.split()
+                E_ZP = float(line_zp[-1])
+            if line.find("Sum of electronic and thermal Energies=") >= 0:
+                line_t = line.split()
+                E_t = float(line_t[-1])
+            if line.find("Sum of electronic and thermal Enthalpies=") >= 0:
+                line_t = line.split()
+                E_enth = float(line_t[-1])
+            if line.find("Sum of electronic and thermal Free Energies=") >= 0:
+                line_free = line.split()
+                E_free = float(line_free[-1])
+            if line.find("KCal/Mol") >= 0:
+                count += 1
+                continue
+            if count == 1:
+                line_ECvS = line.split()
+                Ei = line_ECvS[1]
+                Cv = line_ECvS[2]
+                St= line_ECvS[3]
+                count = 0
         
+        return  Freq, IR_int, Raman_int, E_ZP,  E_t, E_enth, E_free, Ei, Cv, St
+
+    def extract_vibvec(self, lines):
+        result_lines = []
+        vibX = []
+        vibY = []
+        vibZ = []
+        
+        count = 0
+        
+        for line in lines:
+            pattern_match = re.match(r'\s*\bAtom\s+AN\s+X\s+Y\s+Z\s+X\s+Y\s+Z\s+X\s+Y\s+Z\b', line)
+            if pattern_match:
+                count += 1
+            match = re.match(r'\s*(\d+)\s+(\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+', line)
+            if match:
+                vibvec_info = line.strip().split()
+                #print(vibvec_info[2:])
+                for i in range(2, len(vibvec_info), 3): 
+                    vibX.append(float(vibvec_info[i]))
+                    vibY.append(float(vibvec_info[i+1]))
+                    vibZ.append(float(vibvec_info[i+2]))
+        
+                result_lines.append(line)
+        
+        vibX = np.array(vibX)
+        vibY = np.array(vibY)
+        vibZ = np.array(vibZ)
+        
+        vibX_tmp = vibX.reshape(count, -1)
+        vibY_tmp = vibY.reshape(count, -1)
+        vibZ_tmp = vibZ.reshape(count, -1)
+        
+        vibX_reshape = vibX_tmp[0].reshape(-1, 3).T
+        vibY_reshape = vibY_tmp[0].reshape(-1, 3).T
+        vibZ_reshape = vibZ_tmp[0].reshape(-1, 3).T
+        
+        for i in range(1, count):
+            vibX_reshape = np.vstack([vibX_reshape, vibX_tmp[i].reshape(-1, 3).T])
+            vibY_reshape = np.vstack([vibY_reshape, vibY_tmp[i].reshape(-1, 3).T])
+            vibZ_reshape = np.vstack([vibZ_reshape, vibZ_tmp[i].reshape(-1, 3).T])
+        
+        #print(vibX_reshape)
+        #print(vibY_reshape)
+        #print(vibZ_reshape)
+        
+        return vibX_reshape, vibY_reshape, vibZ_reshape
+
+    def extract_polar(self, lines):
+
+        activate_extraction = False
+        iso_values = []
+        aniso_values = []
+        
+        for line in lines:
+           # if line.strip() == "Dipole polarizability, Alpha (dipole orientation).":
+           #     activate_extraction = True
+            if line.strip() == "Dipole polarizability, Alpha (input orientation).":
+                activate_extraction = True
+            if activate_extraction:
+                if line.strip().startswith("iso"):
+                    values = re.findall(r"\d+\.\d+[Dd][\+\-]?\d+", line)
+                    #iso_values.extend(values)
+                    iso_values.extend([float(val.replace('D', 'E')) for val in values])
+                elif line.strip().startswith("aniso"):
+                    values = re.findall(r"\d+\.\d+[Dd][\+\-]?\d+", line)
+                    #aniso_values.extend(values)
+                    aniso_values.extend([float(val.replace('D', 'E')) for val in values])
+        
+        return iso_values, aniso_values
+
     def classify_task(self, lines, charge, spinmulti):
         count = 0
         GScharge = 0
@@ -525,12 +636,33 @@ if __name__ == '__main__':
     job_index, Links_split = parselog.Check_task()
     print(job_index)
 
+    output = {}
+
+    if 'uv_line' in job_index:
+        lines = Links_split[job_index['uv_line']] 
+        _, _, _, State_allowed, State_forbidden, WL_allowed, WL_forbidden, OS_allowed, OS_forbidden, \
+            CD_L_allowed, CD_L_forbidden, CD_OS_allowed, CD_OS_forbidden, \
+            mu_allowed, mu_forbidden, theta_allowed, theta_forbidden, g_allowed, g_forbidden = parselog.Extract_ExcitedState(lines)
+        output["uv"] = [WL_allowed, OS_allowed, CD_L_allowed, CD_OS_allowed]
+        output["state_index"] = [State_allowed, State_forbidden]
+        output["CD_mu"] = [mu_allowed, mu_forbidden]
+        output["CD_theta"] = [theta_allowed, theta_forbidden]
+        output["CD_g"] = [g_allowed, g_forbidden]
+
     if 'relaxAEstate' in job_index:
         lines = Links_split[job_index['relaxAEstate']] 
-        mu, theta, g = parselog.extract_transtion_EMmoment(lines)
-        print(mu)
-        print(theta)
-        print(g)
+        S_Found, S_Egrd, S_Eext, State_allowed, State_forbidden, WL_allowed, WL_forbidden, OS_allowed, OS_forbidden, \
+            CD_L_allowed, CD_L_forbidden, CD_OS_allowed, CD_OS_forbidden, \
+            mu_allowed, mu_forbidden, theta_allowed, theta_forbidden, g_allowed, g_forbidden = parselog.Extract_ExcitedState(lines)
+        # For Check internal coordinate
+        output["MinEtarget"] = S_Eext
+        output["fluor"] = [WL_allowed, OS_allowed, CD_L_allowed, CD_OS_allowed]
+        #mu, theta, g_cal = parse_log.extract_transtion_EMmoment(lines)
+        output["CDL_mu"] = mu_allowed 
+        output["CDL_theta"] = theta_allowed
+        output["CDL_g"] = g_allowed
+
+    print (output)
 
 else:
     from qcforever import gaussian_run
--- qcforever/gaussian_run/parse_log_test.py
+++ qcforever/gaussian_run/parse_log_test.py
@@ -361,6 +361,119 @@ class parse_log:
         return Found, Egrd, Eext, State_allowed, State_forbidden, WL_allowed, WL_forbidden, OS_allowed, OS_forbidden, \
             CD_L_allowed, CD_L_forbidden, CD_OS_allowed, CD_OS_forbidden, \
             mu_allowed, mu_forbidden, theta_allowed, theta_forbidden, g_allowed, g_forbidden  
+
+    def extract_Freq(self, lines):
+        count = 0
+        Freq = []
+        IR_int = []
+        Raman_int = []
+
+        print ("Get information about  Frequencies")
+        for line in lines:
+            if line.find("Frequencies --") >= 0:
+                line_Freq = line.split()
+                for i in range(2, len(line_Freq)):
+                    Freq.append(float(line_Freq[i]))
+            if line.find("IR Inten    --") >= 0:
+                line_ir = line.split()
+                for i in range(3, len(line_ir)):
+                    IR_int.append(float(line_ir[i]))
+            if line.find("Raman Activ --") >= 0:
+                line_raman = line.split()
+                for i in range(3, len(line_raman)):
+                    Raman_int.append(float(line_raman[i]))
+            if line.find("Sum of electronic and zero-point Energies=") >= 0:
+                line_zp = line.split()
+                E_ZP = float(line_zp[-1])
+            if line.find("Sum of electronic and thermal Energies=") >= 0:
+                line_t = line.split()
+                E_t = float(line_t[-1])
+            if line.find("Sum of electronic and thermal Enthalpies=") >= 0:
+                line_t = line.split()
+                E_enth = float(line_t[-1])
+            if line.find("Sum of electronic and thermal Free Energies=") >= 0:
+                line_free = line.split()
+                E_free = float(line_free[-1])
+            if line.find("KCal/Mol") >= 0:
+                count += 1
+                continue
+            if count == 1:
+                line_ECvS = line.split()
+                Ei = line_ECvS[1]
+                Cv = line_ECvS[2]
+                St= line_ECvS[3]
+                count = 0
+        
+        return  Freq, IR_int, Raman_int, E_ZP,  E_t, E_enth, E_free, Ei, Cv, St
+
+    def extract_vibvec(self, lines):
+        result_lines = []
+        vibX = []
+        vibY = []
+        vibZ = []
+        
+        count = 0
+        
+        for line in lines:
+            pattern_match = re.match(r'\s*\bAtom\s+AN\s+X\s+Y\s+Z\s+X\s+Y\s+Z\s+X\s+Y\s+Z\b', line)
+            if pattern_match:
+                count += 1
+            match = re.match(r'\s*(\d+)\s+(\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+', line)
+            if match:
+                vibvec_info = line.strip().split()
+                #print(vibvec_info[2:])
+                for i in range(2, len(vibvec_info), 3): 
+                    vibX.append(float(vibvec_info[i]))
+                    vibY.append(float(vibvec_info[i+1]))
+                    vibZ.append(float(vibvec_info[i+2]))
+        
+                result_lines.append(line)
+        
+        vibX = np.array(vibX)
+        vibY = np.array(vibY)
+        vibZ = np.array(vibZ)
+        
+        vibX_tmp = vibX.reshape(count, -1)
+        vibY_tmp = vibY.reshape(count, -1)
+        vibZ_tmp = vibZ.reshape(count, -1)
+        
+        vibX_reshape = vibX_tmp[0].reshape(-1, 3).T
+        vibY_reshape = vibY_tmp[0].reshape(-1, 3).T
+        vibZ_reshape = vibZ_tmp[0].reshape(-1, 3).T
+        
+        for i in range(1, count):
+            vibX_reshape = np.vstack([vibX_reshape, vibX_tmp[i].reshape(-1, 3).T])
+            vibY_reshape = np.vstack([vibY_reshape, vibY_tmp[i].reshape(-1, 3).T])
+            vibZ_reshape = np.vstack([vibZ_reshape, vibZ_tmp[i].reshape(-1, 3).T])
+        
+        #print(vibX_reshape)
+        #print(vibY_reshape)
+        #print(vibZ_reshape)
+        
+        return vibX_reshape, vibY_reshape, vibZ_reshape
+
+    def extract_polar(self, lines):
+
+        activate_extraction = False
+        iso_values = []
+        aniso_values = []
+        
+        for line in lines:
+           # if line.strip() == "Dipole polarizability, Alpha (dipole orientation).":
+           #     activate_extraction = True
+            if line.strip() == "Dipole polarizability, Alpha (input orientation).":
+                activate_extraction = True
+            if activate_extraction:
+                if line.strip().startswith("iso"):
+                    values = re.findall(r"\d+\.\d+[Dd][\+\-]?\d+", line)
+                    #iso_values.extend(values)
+                    iso_values.extend([float(val.replace('D', 'E')) for val in values])
+                elif line.strip().startswith("aniso"):
+                    values = re.findall(r"\d+\.\d+[Dd][\+\-]?\d+", line)
+                    #aniso_values.extend(values)
+                    aniso_values.extend([float(val.replace('D', 'E')) for val in values])
+        
+        return iso_values, aniso_values
         
     def classify_task(self, lines, charge, spinmulti):
         count = 0
@@ -557,6 +670,12 @@ if __name__ == '__main__':
         output["CDL_mu"] = mu_allowed 
         output["CDL_theta"] = theta_allowed
         output["CDL_g"] = g_allowed
+
+    if 'freq_line' in job_index:
+        lines = Links_split[job_index['freq_line']] 
+        Polar_iso, Polar_aniso = parselog.extract_polar(lines)
+        output["polar_iso"] = Polar_iso[1]
+        output["polar_aniso"] = Polar_aniso[1]
 
     print (output)
 
