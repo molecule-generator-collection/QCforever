946038810ad5b738615ffe85bc4b691e1f70439c
upyo-ms <masato.sumita@riken.jp>
2024-05-24 09:19:12 +0900

    Examples of Gaussian are modified

--- gaussian_main.py
+++ gaussian_main.py
@@ -11,9 +11,9 @@ def main():
     except:
         print (usage); sys.exit()
 
-    #for stargint from gaussian calculation
-    #option = "opt pka symm"
-    option = "freq opt homolumo dipole uv symm satkoopmans"
+    #for starting from gaussian calculation
+    option = "opt uv fluor"
+    #option = "freq opt homolumo dipole uv symm satkoopmans"
     #option = "symm opt freq nmr uv=/home/sumita/GaussianRun_2.2beta/Samples/UV_peak.dat" 
     #option = "opt homolumo energy dipole deen stable2o2 uv=/home/sumita/GaussianRun_2.2beta/Samples/UV_peak.dat vip vea aip aea" 
     #option = "opt homolumo energy dipole deen stable2o2 uv vip vea aip aea" 
@@ -22,16 +22,16 @@ def main():
     #option = "opt nmr uv energy homolumo dipole deen stable2o2 vip vea cden symm"
 
     #test = GaussianRunPack.GaussianDFTRun('B3LYP', 'STO-3G', 10, option, infilename, solvent='water', restart=False)
-    test = GaussianRunPack.GaussianDFTRun('LC-BLYP', 'STO-3G', 10, option, infilename,  restart=False)
+    test = GaussianRunPack.GaussianDFTRun('LTLC-BLYP-BO', 'STO-3G', 10, option, infilename,  restart=False)
 
-    test.para_functional = [0.3]
+    #test.para_functional = [0.1961]
     test.mem = '5GB'
-    test.timexe = 60*60
+    test.timexe = 24*60*60
     #for geometric constrain
     #test.geom_spec = { '1 2 3 14': [180.0, 'F'], '6 5 4 13': [180.0, 'F'], '2 1 7 12': [180.0, 'F'], '5 6 8 9' : [180.0, 'F']}
     #Specify spin multiplicity and charge of the target
-    #test.SpecSpinMulti = 3
-    #test.SpecTotalCharge = 3
+    #test.SpecSpinMulti = 1
+    #test.SpecTotalCharge = 2
     outdic = test.run_gaussian()
 
     print (outdic)
