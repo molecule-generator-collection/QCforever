2ecee1513d9cce34732dde3e7d4ef4ef28d2a272
upyo-ms <masato.sumita@riken.jp>
2024-06-09 09:58:18 +0900

    Add the function waiting resource (core)

--- qcforever/util/check_resource.py
+++ qcforever/util/check_resource.py
@@ -1,14 +1,33 @@
 import sys
 import os
+import time
 
 def respec_cores(spec_cores):
-    ava_cores = len(os.sched_getaffinity(0)) 
+
+    ava_cores = get_ava_cores()
+
     if ava_cores < spec_cores:
         respec_cores = ava_cores
+        print(f'{spec_cores} cores you specifed are not available! \n'
+                f'The number of cores for QC is reduced to {ava_cores}.')
     else:
         respec_cores = spec_cores 
 
     return respec_cores
+
+
+def get_ava_cores():
+
+    ava_cores = len(os.sched_getaffinity(0)) 
+
+    if ava_cores == 0:
+        while ava_cores == 0:
+            time.sleep(10)
+            print ('Waiting resource.....')
+            ava_cores = len(os.sched_getaffinity(0)) 
+
+    return ava_cores
+
 
 if __name__ == '__main__':
     usage = f'Usage; {sys.argv[0]} number_of_cores'
