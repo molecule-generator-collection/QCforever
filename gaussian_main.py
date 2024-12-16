8a8d08f50ebfb76983035c0e967937ef463503f8
upyo-ms <masato.sumita@riken.jp>
2024-12-16 14:41:26 +0900

    Sample use is added

--- gaussian_main.py
+++ gaussian_main.py
@@ -12,25 +12,48 @@ def main():
         print (usage); sys.exit()
 
     #for starting from gaussian calculation
-    option = "opt uv fluor"
-    #option = "freq opt homolumo dipole uv symm satkoopmans"
-    #option = "symm opt freq nmr uv=/home/sumita/GaussianRun_2.2beta/Samples/UV_peak.dat" 
-    #option = "opt homolumo energy dipole deen stable2o2 uv=/home/sumita/GaussianRun_2.2beta/Samples/UV_peak.dat vip vea aip aea" 
-    #option = "opt homolumo energy dipole deen stable2o2 uv vip vea aip aea" 
-    #option = "opt homolumo energy dipole deen stable2o2 fluor=3" 
-    #option = "opt cden homolumo energy dipole deen stable2o2 tadf" 
-    #option = "opt nmr uv energy homolumo dipole deen stable2o2 vip vea cden symm"
+    ##For just computing energy
+    option = "energy"
+    ##For getting the energy after geometry optimization
+    #option = "opt energy"
+    ##For getting the energy and uv after geometry optimization
+    #option = "opt energy uv"
+    ##For getting the energy, uv, and fluorescence  after geometry optimization
+    #option = "opt energy uv fluor"
+    ##For getting the energy, uv, and fluorescence  after geometry optimization with optimization option
+    #option = "opt=loose energy uv fluor"
+    ##For comparing with uv spectrum data after geometry optimization
+    #option = "opt uv=Path2/Samples/UV_peak.dat "
+    ##For computing homo/lumo gap and uv after conformation search
+    #option = "optconf opt homolumo uv"
+    ##For computing homo/lumo gap, dipole moment, decomposition energy, stable to oxygen molecule
+    #option = "opt homolumo energy dipole deen stable2o2" 
+    ##For computing vertical ionization potential/electronic affinity, adiabatic ionization potential/electronic affinity
+    #option = "vip vea aip aea" 
+    ##For computing the fluorescence from the third excited state 
+    #option = "opt fluor=3" 
+    ##For computing the gap between singlet excited state and triplet excited state
+    #option = "opt tadf" 
 
+    ##Making the instance with specifying functional, basis set, number of cores.
+    #B3LYP/STO-3G with 10 cores
+    test = GaussianRunPack.GaussianDFTRun('B3LYP', 'STO-3G', 10, option, infilename, restart=False)
+    #B3LYP/STO-3G with solvent effect after computation chk file will be removed
     #test = GaussianRunPack.GaussianDFTRun('B3LYP', 'STO-3G', 10, option, infilename, solvent='water', restart=False)
-    test = GaussianRunPack.GaussianDFTRun('LTLC-BLYP-BO', 'STO-3G', 10, option, infilename,  restart=False)
+    #KTLC-BLYP-BO/3-21G with 10 cores. After computation, the data will be saved as pkl file
+    #test = GaussianRunPack.GaussianDFTRun('KTLC-BLYP-BO', '3-21G', 10, option, infilename,  restart=False, pklsave=True)
+    #KTLC-BLYP-BO/3-21G with 10 cores. After computation, the data will be saved as pkl file
+    #B3LYP/STO-3G after computation chk file will be removed
+    #test = GaussianRunPack.GaussianDFTRun('KTLC-BLYP-BO', '3-21G', 20, option, infilename, restart=True, pklsave=True)
 
+    #if you want to set the parameter of LC-DFT.
     #test.para_functional = [0.1961]
-    test.mem = '5GB'
-    test.timexe = 24*60*60
+    test.mem = '20GB'
+    test.timexe = 48*60*60
     #for geometric constrain
     #test.geom_spec = { '1 2 3 14': [180.0, 'F'], '6 5 4 13': [180.0, 'F'], '2 1 7 12': [180.0, 'F'], '5 6 8 9' : [180.0, 'F']}
     #Specify spin multiplicity and charge of the target
-    #test.SpecSpinMulti = 1
+    #test.SpecSpinMulti = 3
     #test.SpecTotalCharge = 2
     outdic = test.run_gaussian()
 
