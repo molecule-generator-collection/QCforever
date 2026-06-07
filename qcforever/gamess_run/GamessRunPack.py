bf035acd9891f82464f8d6e03441ad1f011aac66
upyo-ms <masato.sumita@riken.jp>
2026-06-07 16:51:32 +0900

    Replace os.path with pathlib

--- qcforever/gamess_run/GamessRunPack.py
+++ qcforever/gamess_run/GamessRunPack.py
@@ -579,7 +579,7 @@ class GamessDFTRun:
             print('Try to find stable conformation...')
             if ReadFrom == 'sdf' or ReadFrom == 'xyz':
                 print('The input is ready for the conformation search...')
-                original_file = '../' + infilename
+                original_file = Path('..') / infilename
                 try:
                     #laqa_confopt_sdf.LAQA_confopt_main(original_sdf, TotalCharge, SpinMulti, 
                     laqa_confopt_QCforever.LAQA_confopt_main(original_file, TotalCharge, SpinMulti, 
--- qcforever/gaussian_run/GaussianRunPack.py
+++ qcforever/gaussian_run/GaussianRunPack.py
@@ -1169,8 +1169,7 @@ class GaussianDFTRun:
             print('Try to find stable conformation...')
             if ReadFrom == 'sdf' or ReadFrom == 'xyz':
                 print('The input is ready for the conformation search...')
-                #original_sdf = '../' + infilename
-                original_file = '../' + infilename
+                original_file = Path("..") / infilename
                 try:
                     #laqa_confopt_sdf.LAQA_confopt_main(original_sdf, TotalCharge, SpinMulti, 
                     laqa_confopt_QCforever.LAQA_confopt_main(original_file, TotalCharge, SpinMulti, 
